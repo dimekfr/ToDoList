bc31435e59844c6fbc05504ebec41dc1469715a3
Dieu Kengele <dimekfr@yahoo.fr>
2023-01-25 23:43:31 -0300

    adding a simple test just for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterLink } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,7 +9,26 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [ 
+        RouterTestingModule
+      ]
     }).compileComponents();
   });
 
+  it(`should have as title ' '`,async () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;  
+    expect(app.title).toEqual('');
+    
+  });
+  
+  it('Should render title in a h1 tag',async () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('');
+  });
+
 });
+
+
